07244773f24cdf293a8d1d884aa6201582546478
Yoonsung <lvev9925@naver.com>
2015-04-09 00:57:02 +0900

    (P8) Directory basic operation

--- FilePractice/FilePractice/main.m
+++ FilePractice/FilePractice/main.m
@@ -10,80 +10,41 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        //기본 파일작업
-        NSString *fName = @"testfile";
+        //기본 디렉토리 작업
+        NSString *dirName = @"testdir";
+        NSString *path;
         NSFileManager *fm;
-        NSDictionary *attr;
         
         //파일 매니저의 인스턴스를 생성한다.
         fm = [NSFileManager defaultManager];
         
-        NSLog(@"%@", [fm currentDirectoryPath]);
+        //현재 디렉토리를 받아온다.
+        path = [fm currentDirectoryPath];
+        NSLog(@"Current directory path is %@", path);
         
-        //testFile의 존재여부를 먼저 확인한다.
-        if ([fm fileExistsAtPath: fName] == NO) {
-            NSLog(@"File doesn't exist");
+        //새 디렉토리를 생성한다.
+        if ([fm createDirectoryAtPath:dirName withIntermediateDirectories:YES attributes:nil error:NULL] == NO) {
+            NSLog(@"Couldn't create directory!");
             return 1;
         }
         
-        //이제 복사본을 만들자.
-        if ([fm copyItemAtPath:fName toPath:@"newfile" error:NULL] == NO) {
-            NSLog(@"File Copy failed!");
+        //새 디렉토리의 이름을 변경한다.
+        if ([fm moveItemAtPath:dirName toPath:@"newdir" error:NULL] == NO) {
+            NSLog(@"Directory rename failed!");
             return 2;
         }
         
-        //두개의 파일이 일치하는지 확인한다.
-        if ([fm contentsEqualAtPath:fName andPath:@"newfile"] == NO) {
-            NSLog(@"Files are Not Equal!");
+        //새 디렉토리로 현재 디렉토리를 이동한다.
+        if ([fm changeCurrentDirectoryPath:@"newdir"] == NO) {
+            NSLog(@"Change directory failed!");
             return 3;
         }
         
-        //사본의 이름을 변경하자.
-        if ([fm moveItemAtPath:@"newfile" toPath:@"newfile2" error:NULL] == NO) {
-            NSLog(@"File rename Failed!");
-            return 4;
-        }
+        //현재 디렉토리를 받아 표시한다.
+        path = [fm currentDirectoryPath];
+        NSLog(@"Current directory path is %@", path);
         
-        //newfile2의 크기를 알아낸다.
-        if ((attr = [fm attributesOfItemAtPath:@"newfile2" error:NULL]) == nil) {
-            NSLog(@"Couldn't get file attributes!");
-            return 5;
-        }
-        
-        NSLog(@"File size is %llu bytes", [[attr objectForKey:NSFileSize] unsignedLongLongValue]);
-        
-        //원본 파일을 삭제한다.
-        /*
-        if ([fm removeItemAtPath:fName error:NULL] == NO) {
-            NSLog(@"file removal failed");
-            return 6;
-        }
-        */
-        
-        NSLog(@"All operations were successful");
-        
-        //새로 생성한 파일의 내용을 표시한다.
-        NSLog(@"%@", [NSString stringWithContentsOfFile:@"newfile2" encoding:NSUTF8StringEncoding error:NULL]);
-        
-        
-        //데이터를 버퍼에 저장할 경우, NSData , NSMutableData를 이용할 수 있다.
-        NSData *fileData;
-        
-        fileData = [fm contentsAtPath:@"newfile2"];
-        
-        //newfile2 파일을 읽는다.
-        if (fileData == nil) {
-            NSLog(@"File read failed!");
-            return 7;
-        }
-        
-        //newfile3에 데이터를 기록한다.
-        if ([fm createFileAtPath:@"newfile3" contents:fileData attributes:nil] == NO) {
-            NSLog(@"Couldn't create the copy");
-            return 8;
-        }
-        
-        NSLog(@"File copy was successful!");
+        NSLog(@"All operations were successful!");
     }
     return 0;
 }
