c0eaf681cce9555b42ad94ecca00d12410b6275c
Yoonsung <lvev9925@naver.com>
2015-04-08 20:26:53 +0900

    (P7) MutableDictionary recently added syntax

--- DictionaryTest/DictionaryTest/main.m
+++ DictionaryTest/DictionaryTest/main.m
@@ -13,16 +13,16 @@ int main(int argc, const char * argv[]) {
         NSMutableDictionary * glossary = [NSMutableDictionary dictionary];
         
         //소사전에 항목 세개를 저장한다.
-        [glossary setObject:@"A class defined so other classes can inherit from it" forKey:@"abstract class"];
-        [glossary setObject:@"To implement all the methods defined in a protocol" forKey:@"adopt"];
-        [glossary setObject:@"Storing an object for later use" forKey:@"archiving"];
+        glossary[@"abstract class"] = @"A class defined so other classes can inherit from it";
+        glossary[@"adopt"]          =  @"To implement all the methods defined in a protocol";
+        glossary[@"archiving"]     = @"Storing an object for later use";
         
         //항목을 가져와서 보여준다.
-        NSLog(@"absract class: %@", [glossary objectForKey:@"abstract class"]);
-        NSLog(@"adopt : %@", [glossary objectForKey:@"adopt"]);
-        NSLog(@"archiving : %@", [glossary objectForKey:@"archiving"]);
+        NSLog(@"absract class: %@", glossary[@"abstract class"]);
+        NSLog(@"adopt : %@", glossary[@"adopt"]);
+        NSLog(@"archiving : %@", glossary[@"archiving"]);
         
-        NSLog(@"not : %@", [glossary objectForKey:@"not"]);
+        NSLog(@"not : %@", glossary[@"not"]);
     }
     return 0;
 }
